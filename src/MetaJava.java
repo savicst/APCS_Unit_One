9b4b03df5f7667021e956e500967b1282c0fd393
savicst <savicst@ssfs.org>
2021-09-07 12:00:15 -0400

    idk

--- src/MetaJava.java
+++ src/MetaJava.java
@@ -1,6 +1,11 @@
 public class MetaJava {
 
     public static void main(String[] args) {
+        System.out.println("public class Hello {");
+        System.out.println("    public static void main(String[] args) {");
+        System.out.println("        System.out.println(\"Hello, world!\");\n");
+        System.out.println("    }");
+        System.out.print("}");
 
     }
 }
