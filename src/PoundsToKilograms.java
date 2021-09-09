81c1aafb1ba81667b58f38dfd8143276f6f79786
savicst <savicst@ssfs.org>
2021-09-09 10:54:07 -0400

    Completed 3 1.3 tasks.

--- src/PoundsToKilograms.java
+++ src/PoundsToKilograms.java
@@ -2,6 +2,9 @@ public class PoundsToKilograms {
 
     public static void main(String[] args)
     {
+        double lbs = 115.0;
+        double kg = (lbs/2.205);
+        System.out.println("115.0 pounds = "+kg+ " kilograms");
         //declare the variables
         //assign the value 115 to lbs
         //calculate the kilograms and assign that value to kg
--- src/RectangleArea.java
+++ src/RectangleArea.java
@@ -3,6 +3,9 @@ public class RectangleArea {
 
     public static void main(String[] args) {
         int length = 14;
+        int width = 6;
+        int area = (length*width);
+        System.out.println("Area = "+area);
         //declare and initialize width
 
         //declare area - use length and width to calculate area
