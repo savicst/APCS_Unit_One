4f90cd4a57cd4e2f20effe71e67bb58c32cebf8e
savicst <savicst@ssfs.org>
2021-09-09 10:59:10 -0400

    Completed 3 1.3 tasks.

--- src/TotalWithTax.java
+++ src/TotalWithTax.java
@@ -2,6 +2,14 @@ public class TotalWithTax {
 
     public static void main(String[] args)
     {
+        final double TAXRATE = 0.0825;
+        double price = 52.75;
+        double tax = (price * TAXRATE);
+        double total = (price + tax);
+        System.out.println("Price: $" + price);
+        System.out.println();
+        System.out.println();
+
         //Declare the variables and the constant.
         //Make the calculations and assign
         //the results to the appropriate variables.
