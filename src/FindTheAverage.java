63e47424a624dfefe5c8f2045a4ca14fa635cf3e
savicst <savicst@ssfs.org>
2021-09-14 17:30:12 -0400

    Completed FindTheAverage

--- src/FindTheAverage.java
+++ src/FindTheAverage.java
@@ -10,22 +10,22 @@ public class FindTheAverage {
         double total;
             // Get four pieces of user input
         System.out.println("Enter four values.");
-        System.out.println("Value 1: ");
+        System.out.print("Value 1: ");
         double val1 = scan.nextDouble();
         total = val1;
-        System.out.println("Value 2: ");
+        System.out.print("Value 2: ");
         double val2 = scan.nextDouble();
         total += val2;
-        System.out.println("Value 3: ");
+        System.out.print("Value 3: ");
         double val3 = scan.nextDouble();
         total += val3;
-        System.out.println("Value 4: ");
+        System.out.print("Value 4: ");
         double val4 = scan.nextDouble();
         total += val4;
             // Calculate the average
         total /= 4;
             // Display the result
-        System.out.println("Average: " + total);
+        System.out.println("Average = " + total);
 
 
     }
